959796adb4a89bf97a1f131ff522b5e71b2184a2
Nirajan Bist <bistneon@gmail.com>
2022-04-13 14:58:18 +0545

    Made changes suitable for Heroku Deployment
    + Added engines for node and npm in package.json
    + Added start script: `node index.js`
    + Assign PORT variable to listen from environment variables.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.get("/", (req,res)=>{
     res.json({data:"Hi"})
 })
 
-const port = 4000;
+const port = process.env.PORT || 4000;
+
 app.listen(port,()=>{
     console.log(`Listening on port ${port}...`);
 });
