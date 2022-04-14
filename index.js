bbb2440913e9c1a66276dbe582e831f2a0e415c4
Nirajan Bist <bistneon@gmail.com>
2022-04-15 05:06:45 +0545

    User model created

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ require("./services/passport");
 //DB connection
 mongoose.connect(keys.mongodbURI);
 
+require("./models/User");
+
 app.use('/auth', require("./routes/authRoutes"));
 
 const port = process.env.PORT || 4000;
