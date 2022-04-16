acd63e9f82bfaa7e2a6825f1a109b73ec9c09c29
Nirajan Bist <bistneon@gmail.com>
2022-04-17 00:22:23 +0545

    create-store with dummy reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(
+  () => [], //dummy reducer
+  {}, //dummy initialState
+  applyMiddleware() //middlewares
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
