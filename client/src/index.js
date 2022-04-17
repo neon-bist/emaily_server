f77ea0a477c91ed730ebfe219b5c33d32f1260db
Nirajan Bist <bistneon@gmail.com>
2022-04-17 10:44:32 +0545

    reducers setup

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
 
 const store = createStore(
-  () => [], //dummy reducer
+  reducers, //dummy reducer
   {}, //dummy initialState
   applyMiddleware() //middlewares
 );
