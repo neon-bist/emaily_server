ffc192cd19557dc8a337d96618f85ef1e3f25dda
Nirajan Bist <bistneon@gmail.com>
2022-04-17 15:33:12 +0545

    Frontend setup for payment actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,3 +5,8 @@ export const fetchUser = () => async (dispatch) => {
   const { data } = await axios.get("/api/current_user");
   dispatch({ type: FETCH_USER, payload: data });
 };
+
+export const handleToken = (token) => async (dispatch) => {
+  const { data } = await axios.post("/api/stripe", { token });
+  dispatch({ type: FETCH_USER, payload: data });
+};
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,14 +1,16 @@
 import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-const Payments = () => (
+const Payments = (props) => (
   <StripeCheckout
     name="Emaily"
     description="$5 for 5 credits"
     amount={5}
-    token={(token) => console.log(token)}
+    token={(token) => props.handleToken(token)}
     stripeKey={process.env.REACT_APP_STRIPE_KEY}
   >
     <button className="btn">Add Credits</button>
   </StripeCheckout>
 );
-export default Payments;
+export default connect(null, actions)(Payments);
