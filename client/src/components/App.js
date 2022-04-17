c4beab45df5778114d46ef64562188d02ad99a0c
Nirajan Bist <bistneon@gmail.com>
2022-04-17 11:23:26 +0545

    materialize-css installed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-const Header = () => <h2>Header</h2>;
+import Header from "./Header";
+
 const Landing = () => <h2>Landing</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
@@ -11,7 +12,10 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" component={Header} />
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveyNew} />
         </div>
       </BrowserRouter>
     </div>
