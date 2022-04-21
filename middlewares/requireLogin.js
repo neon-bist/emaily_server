55608680ba3eea656262e256eece07fd194e6b05
Nirajan Bist <bistneon@gmail.com>
2022-04-21 22:54:26 +0545

    redirect logic enhanced in passport strategy

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
   if (!req.user) {
-    return res.status(401).send("Please login first!");
+    return res.redirect("/auth/google?redirectTo=" + req.baseUrl + req.path);
+    // return res.status(401).send("Please login first!");
   }
   next();
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,37 @@
 const app = require("express").Router();
 const passport = require("passport");
 
-app.get(
-  "/google",
-  passport.authenticate("google", {
+app.get("/google", (req, res, next) => {
+  const { redirectTo } = req.query;
+  const state = redirectTo
+    ? Buffer.from(JSON.stringify({ redirectTo })).toString("base64")
+    : undefined;
+
+  console.log(redirectTo, state);
+  return passport.authenticate("google", {
     scope: ["profile", "email"],
     prompt: "select_account",
-  })
-);
+    state,
+  })(req, res, next);
+});
 
-app.get("/google/callback", passport.authenticate("google"), (req, res) =>
-  res.redirect("/surveys")
+app.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    try {
+      const { state } = req.query;
+      const { redirectTo } = JSON.parse(
+        Buffer.from(state, "base64").toString()
+      );
+      if (typeof redirectTo === "string" && redirectTo.startsWith("/")) {
+        return res.redirect(redirectTo);
+      }
+    } catch {
+      // just redirect normally below
+    }
+    res.redirect("/surveys");
+  }
 );
 
 module.exports = app;
